17a7942a17073a6490438a25833c9a4559de93db
Jim Simon <jim.j.simon@gmail.com>
2025-09-23 02:37:45 -0400

    Explicitly add relations when creating db connection

--- packages/api/src/db/otcgs/index.ts
+++ packages/api/src/db/otcgs/index.ts
@@ -2,7 +2,9 @@ import { drizzle } from "drizzle-orm/libsql";
 import { databaseFile } from "./drizzle.config";
 import { databaseFile as tcgDataDatabaseFile } from "../tcg-data/drizzle.config";
 import * as schema from "./schema";
+import * as relations from "./relations";
 import * as tcgDataSchema from "../tcg-data/schema";
+import * as tcgDataRelations from "../tcg-data/relations";
 import { sql } from "drizzle-orm";
 export * from "./schema";
 
@@ -10,6 +12,8 @@ const otcgs = drizzle(databaseFile, {
   schema: {
     ...schema,
     ...tcgDataSchema,
+    ...relations,
+    ...tcgDataRelations,
   },
 });
 otcgs.run(sql`ATTACH_DATABASE '${tcgDataDatabaseFile}' AS tcg_data;`);
